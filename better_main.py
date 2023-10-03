ee796d4907e68c1e7c0e0abe94459fac236ac157
arthurdesma <arthurchamonix@gmail.com>
2023-10-03 14:17:00 +0200

    mise a jour 03/10/23

--- better_main.py
+++ better_main.py
@@ -5,34 +5,34 @@ from dash import html
 from dash.dependencies import Input, Output
 import pandas as pd
 
-# Importing required modules
+# Importation des modules nécessaires
 from gas_station_density import generate_gas_station_density_map
 from fuel_prices import generate_fuel_prices_map
 from graph import update_gas_price_graph
 from heat_map import generate_map, fuel_types
 
-# Load data
+# Charger les données
 df2 = pd.read_csv("prix-des-carburants-en-france-flux-instantane-v2.csv", delimiter=";", dtype={'Code postal': str})
-departments = sorted(df2['Code postal'].astype(str).str[:2].unique())
-options = [{'label': dept, 'value': dept} for dept in departments]
+departements = sorted(df2['Code postal'].astype(str).str[:2].unique())
+options = [{'label': dept, 'value': dept} for dept in departements]
 
 df1 = pd.read_csv('PrixCarburants_annuel_2022.csv')
 grouped_df = df1.groupby(['Date', 'Gas_Type'])['Price'].mean().reset_index()
 
-# Initialize Dash app
+# Initialiser l'application Dash
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 map_files = [generate_map(fuel) for fuel in fuel_types]
 
 app.layout = dbc.Container([
 
-    dbc.Row(html.H1("Gas Station Analysis in France", className="text-center mb-5"), className="mb-4"),
+    dbc.Row(html.H1("Analyse des stations-service en France", className="text-center mb-5"), className="mb-4"),
 
     dbc.Row(dbc.Col(dbc.Card(
         [
-            dbc.CardHeader(html.H4("Fuel Prices by Department", className="card-title text-center")),
+            dbc.CardHeader(html.H4("Prix du carburant par département", className="card-title text-center")),
             dbc.CardBody([
-                html.P("Choose a department to see average fuel prices. The map updates based on the selected department.", className="text-muted text-center mb-4"),
+                html.P("Choisissez un département pour voir les prix moyens du carburant. La carte se met à jour selon le département sélectionné.", className="text-muted text-center mb-4"),
                 dbc.Row(dbc.Col(dcc.Dropdown(id='department-dropdown', options=options, value='06', className="mb-3"), width=8), justify="center"),
                 html.Iframe(id="fuel-prices-map", srcDoc=generate_fuel_prices_map('06'), width="100%", height="600", className="border-0")
             ])
@@ -42,9 +42,9 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(dbc.Card(
             [
-                dbc.CardHeader(html.H4("Gas Station Density", className="card-title text-center")),
+                dbc.CardHeader(html.H4("Densité des stations-service", className="card-title text-center")),
                 dbc.CardBody([
-                    html.P("This map shows the density of gas stations across France. Denser regions indicate more gas stations in that locality.", className="text-muted text-center mb-4"),
+                    html.P("Cette carte montre la densité des stations-service à travers la France. Les régions plus denses indiquent un plus grand nombre de stations-service dans cette localité.", className="text-muted text-center mb-4"),
                     html.Iframe(id='gas-density-map', srcDoc=generate_gas_station_density_map(), width='100%', height='600', className="border-0")
                 ])
             ]
@@ -52,9 +52,9 @@ app.layout = dbc.Container([
 
         dbc.Col(dbc.Card(
             [
-                dbc.CardHeader(html.H4('Gas Price over Time', className="card-title text-center")),
+                dbc.CardHeader(html.H4('Prix du carburant au fil du temps', className="card-title text-center")),
                 dbc.CardBody([
-                    html.P("Select the gas type from the dropdown to view its average price trend over time.", className="text-muted text-center mb-4"),
+                    html.P("Sélectionnez le type de carburant dans le menu déroulant pour voir son évolution de prix moyenne au fil du temps.", className="text-muted text-center mb-4"),
                     dcc.Dropdown(
                         id='gas-type-dropdown',
                         options=[{'label': gas_type, 'value': gas_type} for gas_type in grouped_df['Gas_Type'].unique()],
@@ -68,10 +68,10 @@ app.layout = dbc.Container([
         ), md=6, className="shadow-sm")
     ], className="mb-4"),
 
-    # Heat map section with card layout
+    # Section de la carte thermique avec mise en page de la carte
     dbc.Row(dbc.Col(dbc.Card(
     [
-        dbc.CardHeader(html.H4("Average Fuel Price per Department in France", className="card-title text-center")),
+        dbc.CardHeader(html.H4("Prix moyen du carburant par département en France", className="card-title text-center")),
         dbc.CardBody(
             html.Div(
                 [
@@ -81,9 +81,9 @@ app.layout = dbc.Container([
                             html.Iframe(srcDoc=open(map_file, 'r').read(), width='100%', height='600', className="border-0")
                         ],
                         className="mb-4",
-                        style={"width": "33%", "height": "600px"}  # Here we're setting the size of each map's container.
+                        style={"width": "33%", "height": "600px"}  # Ici, nous définissons la taille du conteneur de chaque carte.
                     )
-                    #zip() = combine 2 lists
+                    # zip() = combine 2 listes
                     for fuel, map_file in zip(fuel_types, map_files)
                 ],
                 style={"display": "flex", "flexDirection": "row", "flexWrap": "wrap", "justifyContent": "space-between"}
@@ -100,16 +100,16 @@ app.layout = dbc.Container([
     Output('fuel-prices-map', 'srcDoc'),
     [Input('department-dropdown', 'value')]
 )
-def update_map(selected_department):
-    return generate_fuel_prices_map(selected_department)
+def update_map(departement_selectionne):
+    return generate_fuel_prices_map(departement_selectionne)
 
 @app.callback(
     Output('line-plot', 'figure'),
     [Input('gas-type-dropdown', 'value')]
 )
-def update_graph(selected_gas_type):
-    return update_gas_price_graph(selected_gas_type)
+def update_graph(type_carburant_selectionne):
+    return update_gas_price_graph(type_carburant_selectionne)
 
-# Main
+# Principal
 if __name__ == '__main__':
     app.run_server(debug=True)
